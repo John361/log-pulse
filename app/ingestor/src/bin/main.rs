0bfbdea565ea8f3cba94bd3369c444a463c87bfd
John H <hxjwork1@gmail.com>
2026-01-06 00:36:11 +0400

    Implementing simple batching

--- app/ingestor/src/bin/main.rs
+++ app/ingestor/src/bin/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use tokio::sync::mpsc;
+use tokio::time::{interval, Duration};
 
 use lib::grpc::log::LogEntryRequest;
 use lib::infra_inbound_grpc::service::GrpcServer;
@@ -8,11 +9,33 @@ use lib::infra_inbound_grpc::service::GrpcServer;
 async fn main() -> Result<()> {
     init_tracing();
 
-    let (tx, mut rx) = mpsc::channel::<LogEntryRequest>(10000);
+    let batch_capacity = 100;
+    let buffer_size = 10000;
+    let flush_interval_seconds = 5;
+
+    let (tx, mut rx) = mpsc::channel::<LogEntryRequest>(buffer_size);
 
     tokio::spawn(async move {
-        while let Some(log) = rx.recv().await {
-            tracing::info!("Storage worker received log from: {}", log.service_name);
+        let mut batch = Vec::with_capacity(batch_capacity);
+        let mut flush_interval = interval(Duration::from_secs(flush_interval_seconds));
+
+        loop {
+            tokio::select! {
+                Some(log) = rx.recv() => {
+                    batch.push(log);
+                    if batch.len() >= batch_capacity {
+                        tracing::info!("Batch full, flushing...");
+                        flush_batch(&mut batch).await;
+                    }
+                }
+
+                _ = flush_interval.tick() => {
+                    if !batch.is_empty() {
+                        tracing::info!("Flushing batch due to interval...");
+                        flush_batch(&mut batch).await;
+                    }
+                }
+            }
         }
     });
 
@@ -33,4 +56,12 @@ fn init_tracing() {
         .init();
 
     tracing::debug!("Tracing initialized");
+}
+
+async fn flush_batch(batch: &mut Vec<LogEntryRequest>) {
+    let count = batch.len();
+
+    tracing::info!("--- Flushing batch of {} logs to storage ---", count);
+
+    batch.clear();
 }
